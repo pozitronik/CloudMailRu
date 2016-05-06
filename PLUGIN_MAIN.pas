769bc804e4301ca54bedb0c2461d78b1c64ea218
Pavel Dubrovsky <antikillerxxl@gmail.com>
2016-05-06 12:03:11 +0300

    -todo

--- PLUGIN_MAIN.pas
+++ PLUGIN_MAIN.pas
@@ -15,7 +15,7 @@ implementation
 
 function FindTCWindow: HWND;
 begin
-	Result := FindWindow('TTOTAL_CMD', nil); { TODO : При нескольких запущенных тоталах получать нужный хендл }
+	Result := FindWindow('TTOTAL_CMD', nil); { Хендл отдаётся корректно даже при нескольких запущенных тоталах }
 end;
 
 end.
