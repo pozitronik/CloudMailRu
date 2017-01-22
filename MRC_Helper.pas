133316bf811c33d2e95099f9237b48a9d974aa21
Pavel Dubrovsky <antikillerxxl@gmail.com>
2017-01-22 15:50:22 +0300

    CombineIcons function

--- MRC_Helper.pas
+++ MRC_Helper.pas
@@ -34,6 +34,7 @@ function IsWriteable(const DirName: WideString; FileName: WideString = 'delete.m
 function PosLast(Substring, S: WideString; Offset: Integer = 0): Integer;
 function PathToUrl(path: WideString; RestrictEmptyUrl: boolean = true): WideString;
 function GetFolderIcon(): Hicon;
+function CombineIcons(FrontIcon, BackIcon: Hicon): Hicon; //taken from http://www.swissdelphicenter.ch/en/showcode.php?id=1636
 
 implementation
 
@@ -263,6 +264,46 @@ begin
 		exit(SFI.Hicon);
 	end
 	else RaiseLastOSError;
+end;
+
+function CombineIcons(FrontIcon, BackIcon: Hicon): Hicon;
+var
+	WinDC: HDC;
+	FrontInfo: TIconInfo;
+	FrontDC: HDC;
+	FrontSv: HBITMAP;
+	BackInfo: TIconInfo;
+	BackDC: HDC;
+	BackSv: HBITMAP;
+	BmpObj: tagBitmap;
+begin
+	WinDC := GetDC(0);
+
+	GetIconInfo(FrontIcon, FrontInfo);
+	FrontDC := CreateCompatibleDC(WinDC);
+	FrontSv := SelectObject(FrontDC, FrontInfo.hbmMask);
+
+	GetIconInfo(BackIcon, BackInfo);
+	BackDC := CreateCompatibleDC(WinDC);
+	BackSv := SelectObject(BackDC, BackInfo.hbmMask);
+
+	GetObject(FrontInfo.hbmMask, SizeOf(BmpObj), @BmpObj);
+	BitBlt(BackDC, 0, 0, BmpObj.bmWidth, BmpObj.bmHeight, FrontDC, 0, 0, SRCAND);
+
+	SelectObject(BackDC, BackInfo.hbmColor);
+	DrawIconEx(BackDC, 0, 0, FrontIcon, 0, 0, 0, 0, DI_NORMAL);
+
+	Result := CreateIconIndirect(BackInfo);
+
+	SelectObject(FrontDC, FrontSv);
+	DeleteDC(FrontDC);
+	SelectObject(BackDC, BackSv);
+	DeleteDC(BackDC);
+	ReleaseDC(0, WinDC);
+	DeleteObject(FrontInfo.hbmColor);
+	DeleteObject(FrontInfo.hbmMask);
+	DeleteObject(BackInfo.hbmColor);
+	DeleteObject(BackInfo.hbmMask);
 end;
 
 end.
