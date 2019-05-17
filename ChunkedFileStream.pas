d1cda515d05a1223b491d0fcc9db1e549241cba4
Pozitronik <Pozitronik>
2019-05-17 15:52:34 +0300

    ChunkedFileStream class fully rewritten (based on Ondrej Kelle solution) => seems to be working

--- ChunkedFileStream.pas
+++ ChunkedFileStream.pas
@@ -6,15 +6,20 @@ uses System.Classes, System.SysUtils;
 
 type
 
-	TChunkedFileStream = class(TBufferedFileStream)
+	TChunkedFileStream = class(TStream)
 	private
-		ChunkStart, ChunkSize, Position: Int64;
+		FStartPos, FSize: Int64;
+		FileStream: TFileStream;
 	protected
 		function GetSize(): Int64; override;
 	public
 		property Size: Int64 read GetSize;
+
 		constructor Create(const AFileName: string; Mode: Word; ChunkStart, ChunkSize: Int64);
-		function Read(var Buffer; Count: Longint): Longint; overload; override;
+		destructor Destroy; override;
+		function Read(var Buffer; Count: Longint): Longint; override;
+		function Write(const Buffer; Count: Longint): Longint; override;
+		function Seek(const Offset: Int64; Origin: TSeekOrigin): Int64; override;
 	end;
 
 implementation
@@ -22,41 +27,70 @@ implementation
 {TChunkedFileStream}
 
 constructor TChunkedFileStream.Create(const AFileName: string; Mode: Word; ChunkStart, ChunkSize: Int64);
+var
+
+	FileSize: Int64;
 begin
-	self.ChunkStart := ChunkStart;
-	self.ChunkSize := ChunkSize;
+	inherited Create;
+	self.FileStream := TFileStream.Create(AFileName, Mode);
+	FileSize := FileStream.Size;
+	if (ChunkSize < 0) or (ChunkSize > FileSize) or (ChunkStart < 0) or (ChunkStart > FileSize) or (FileStream.Seek(ChunkStart, soBeginning) <> ChunkStart) then
+		raise EReadError.Create('Can''t read from ' + AFileName + ' ' + ChunkSize.ToString + ' bytes at ' + ChunkStart.ToString);
+	self.FStartPos := ChunkStart;
+	if ChunkSize = 0 then
+		self.FSize := FileSize - ChunkStart
+	else
+		self.FSize := ChunkSize;
 
-	inherited Create(AFileName, Mode);
-	self.Position := 0;;
+end;
 
+destructor TChunkedFileStream.Destroy;
+begin
+	FileStream.Free;
+	inherited Destroy;
 end;
 
 function TChunkedFileStream.GetSize: Int64;
 begin
-	result := ChunkSize;
+	result := FSize;
 end;
 
-function TChunkedFileStream.Read(var Buffer; Count: Integer): Longint;
-var
-	fileStartPosition, fileEndPosition: Int64;
-	BytesToRead: Int64;
-
+function TChunkedFileStream.Read(var Buffer; Count: Longint): Longint;
 begin
-	fileStartPosition := self.Position + self.ChunkStart; //позиция чтения из реального файла
-	fileEndPosition := self.ChunkStart + self.ChunkSize; //позиция конца чтения из реального файла
-
-	if fileStartPosition >= fileEndPosition then
-		Exit(0); //Пытаемся читать за границей чанка
-
-	if Count > self.ChunkSize then
-		BytesToRead := self.ChunkSize
+	if FileStream.Position + Count > self.FStartPos + self.FSize then
+		Count := self.FStartPos + self.FSize - FileStream.Position;
+	if Count > 0 then
+		result := FileStream.Read(Buffer, Count)
 	else
-		BytesToRead := Count; //Количество байт для прочтения
-
-	inherited Seek(fileStartPosition, soBeginning);
-	result := inherited Read(Buffer, BytesToRead);
-	self.Position := self.Position + result;
+		result := 0;
 end;
 
+function TChunkedFileStream.Seek(const Offset: Int64; Origin: TSeekOrigin): Int64;
+var
+	NewPos: Int64;
+begin
+	case Origin of
+		soBeginning:
+			result := FileStream.Seek(FStartPos + Offset, soBeginning) - FStartPos;
+		soCurrent:
+			begin
+				NewPos := FileStream.Position + Offset - FStartPos;
+				if NewPos < 0 then
+					NewPos := 0;
+				if NewPos > FSize then
+					NewPos := FSize;
+				result := FileStream.Seek(FStartPos + NewPos, soBeginning) - FStartPos;
+			end;
+		soEnd:
+			result := FileStream.Seek(FStartPos + FSize + Offset, soBeginning) - FStartPos;
+		else
+			result := -1;
+	end;
+end;
+
+function TChunkedFileStream.Write(const Buffer; Count: Longint): Longint;
+begin
+	result := 0; //too lazy to implement
+end;
 
 end.
