7f04fee555666f4cab19061e7fa0f446fcbfd194
Pozitronik <Pozitronik>
2019-05-20 15:34:43 +0300

    Changed initialization principle: connection manager proxyfies required plugin parameters to cloud class as TCloudSettings

--- ConnectionManager.pas
+++ ConnectionManager.pas
@@ -18,13 +18,7 @@ type
 	private
 		Connections: array of TNamedConnection;
 		IniFileName: WideString;
-		Proxy: TProxySettings;
-		UploadBPS: integer;
-		DownloadBPS: integer;
-		Timeout: integer;
-		CloudMaxFileSize: integer;
-		PrecalculateHash: boolean;
-		CheckCRC: boolean;
+		PluginSettings: TPluginSettings; //Сохраняем параметры плагина, чтобы проксировать параметры из них при инициализации конкретного облака
 
 		ProgressHandleProc: TProgressHandler;
 		LogHandleProc: TLogHandler;
@@ -36,11 +30,11 @@ type
 		function new(connectionName: WideString): integer; //Добавляет подключение в пул
 
 	public
-		constructor Create(IniFileName: WideString; ProxySettings: TProxySettings; Timeout, CloudMaxFileSize, UploadBPS, DownloadBPS: integer; PrecalculateHash: boolean; CheckCRC: boolean; ProgressHandleProc: TProgressHandler; LogHandleProc: TLogHandler; RequestHandleProc: TRequestHandler; PasswordManager: TTCPasswordManager);
+		constructor Create(IniFileName: WideString; PluginSettings: TPluginSettings; ProgressHandleProc: TProgressHandler; LogHandleProc: TLogHandler; RequestHandleProc: TRequestHandler; PasswordManager: TTCPasswordManager);
 		destructor Destroy(); override;
 		function get(connectionName: WideString; var OperationResult: integer; doInit: boolean = true): TCloudMailRu; //возвращает готовое подклчение по имени
 		function set_(connectionName: WideString; cloud: TCloudMailRu): boolean;
-		function init(connectionName: WideString; ProxySettings: TProxySettings; Timeout: integer): integer; //инициализирует подключение по его имени, возвращает код состояния
+		function init(connectionName: WideString): integer; //инициализирует подключение по его имени, возвращает код состояния
 		function free(connectionName: WideString): integer; //освобождает подключение по его имени, возвращает код состояния
 		function freeAll: integer; //освобождает все подключения
 		function initialized(connectionName: WideString): boolean; //Проверяет, инициализировано ли подключение
@@ -50,7 +44,7 @@ type
 implementation
 
 {TConnectionManager}
-constructor TConnectionManager.Create(IniFileName: WideString; ProxySettings: TProxySettings; Timeout, CloudMaxFileSize, UploadBPS, DownloadBPS: integer; PrecalculateHash: boolean; CheckCRC: boolean; ProgressHandleProc: TProgressHandler; LogHandleProc: TLogHandler; RequestHandleProc: TRequestHandler; PasswordManager: TTCPasswordManager);
+constructor TConnectionManager.Create(IniFileName: WideString; PluginSettings: TPluginSettings; ProgressHandleProc: TProgressHandler; LogHandleProc: TLogHandler; RequestHandleProc: TRequestHandler; PasswordManager: TTCPasswordManager);
 begin
 	SetLength(Connections, 0);
 	self.IniFileName := IniFileName;
@@ -58,13 +52,6 @@ begin
 	self.ProgressHandleProc := ProgressHandleProc;
 	self.LogHandleProc := LogHandleProc;
 	self.RequestHandleProc := RequestHandleProc;
-	self.Proxy := ProxySettings;
-	self.Timeout := Timeout;
-	self.CloudMaxFileSize := CloudMaxFileSize;
-	self.UploadBPS := UploadBPS;
-	self.DownloadBPS := DownloadBPS;
-	self.PrecalculateHash := PrecalculateHash;
-	self.CheckCRC := CheckCRC;
 
 	self.PasswordManager := PasswordManager;
 end;
@@ -90,7 +77,7 @@ begin
 	begin
 		OperationResult := CLOUD_OPERATION_OK;
 		if not initialized(connectionName) then
-			OperationResult := init(connectionName, self.Proxy, self.Timeout);
+			OperationResult := init(connectionName);
 		if (OperationResult = CLOUD_OPERATION_OK) then
 			result := get(connectionName, OperationResult, false);
 	end;
@@ -108,31 +95,31 @@ begin
 	result := true;
 end;
 
-function TConnectionManager.init(connectionName: WideString; ProxySettings: TProxySettings; Timeout: integer): integer;
+function TConnectionManager.init(connectionName: WideString): integer;
 var
 	cloud: TCloudMailRu;
-	AccountSettings: TAccountSettings;
+	CloudSettings: TCloudSettings;
 	LoginMethod: integer;
 begin
 	result := CLOUD_OPERATION_OK;
-	AccountSettings := GetAccountSettingsFromIniFile(IniFileName, connectionName);
+	CloudSettings.AccountSettings := GetAccountSettingsFromIniFile(IniFileName, connectionName);
 
-	if not PasswordManager.GetAccountPassword(AccountSettings) then
+	if not PasswordManager.GetAccountPassword(CloudSettings.AccountSettings) then
 		exit(CLOUD_OPERATION_ERROR_STATUS_UNKNOWN); //INVALID_HANDLE_VALUE
 
-	if AccountSettings.encrypt_files_mode <> EncryptModeNone then
+	if CloudSettings.AccountSettings.encrypt_files_mode <> EncryptModeNone then
 	begin
-		if not PasswordManager.InitCloudCryptPasswords(AccountSettings) then
+		if not PasswordManager.InitCloudCryptPasswords(CloudSettings.AccountSettings) then
 			exit(CLOUD_OPERATION_FAILED);
 	end;
 
 	LogHandleProc(LogLevelConnect, MSGTYPE_CONNECT, PWideChar('CONNECT \' + connectionName));
 
-	cloud := TCloudMailRu.Create(AccountSettings, self.CloudMaxFileSize, self.Proxy, Timeout, self.UploadBPS, self.DownloadBPS, self.PrecalculateHash, self.CheckCRC, ProgressHandleProc, LogHandleProc, RequestHandleProc);
+	cloud := TCloudMailRu.Create(CloudSettings, ProgressHandleProc, LogHandleProc, RequestHandleProc);
 	if not set_(connectionName, cloud) then
 		exit(CLOUD_OPERATION_ERROR_STATUS_UNKNOWN); //INVALID_HANDLE_VALUE
 
-	if (AccountSettings.twostep_auth) then
+	if (CloudSettings.AccountSettings.twostep_auth) then
 		LoginMethod := CLOUD_AUTH_METHOD_TWO_STEP
 	else
 		LoginMethod := CLOUD_AUTH_METHOD_WEB;
@@ -142,8 +129,6 @@ begin
 		result := CLOUD_OPERATION_FAILED;
 		free(connectionName);
 	end;
-
-	//cloud.Destroy;
 end;
 
 function TConnectionManager.initialized(connectionName: WideString): boolean;
