5f1573935f335532e8f1666e93ce96ec409aa1a8
Pozitronik <Pozitronik>
2019-05-22 13:27:35 +0300

    HTTP connections management class (issue #209)

---
+++ HTTPManager.pas
@@ -0,0 +1,59 @@
+unit HTTPManager;
+
+interface
+
+uses CMLHTTP, CMLTypes, PLUGIN_TYPES, Settings, System.Generics.Collections, SysUtils, IdCookieManager;
+
+type
+
+	THTTPManager = class
+	private
+		ConnectionSettings: TConnectionSettings;
+		ExternalProgressProc: TProgressHandler;
+		ExternalLogProc: TLogHandler;
+
+		Connections: TDictionary<Cardinal, TCloudMailRuHTTP>; //<ThreadId, HTTP>
+
+	public
+		{ѕараметры, с которыми будут отдаватьс€ подключени€: создаЄм с ними экземпл€р класса, а дальше он сам рулит}
+		constructor Create(Settings: TConnectionSettings; ExternalProgressProc: TProgressHandler = nil; ExternalLogProc: TLogHandler = nil);
+		destructor Destroy; override;
+		function get(ThreadId: Cardinal; doInit: boolean = true): TCloudMailRuHTTP;
+	end;
+
+implementation
+
+{THTTPManager}
+
+constructor THTTPManager.Create(Settings: TConnectionSettings; ExternalProgressProc: TProgressHandler = nil; ExternalLogProc: TLogHandler = nil);
+begin
+	self.ConnectionSettings := Settings;
+	self.ExternalProgressProc := ExternalProgressProc;
+	self.ExternalLogProc := ExternalLogProc;
+	Connections := TDictionary<Cardinal, TCloudMailRuHTTP>.Create;
+end;
+
+destructor THTTPManager.Destroy;
+var
+	Item: TPair<Cardinal, TCloudMailRuHTTP>;
+begin
+	for Item in Connections do
+		Item.Value.Destroy;
+
+	FreeAndNil(Connections);
+	inherited;
+end;
+
+function THTTPManager.get(ThreadId: Cardinal; doInit: boolean = true): TCloudMailRuHTTP;
+begin
+	if not Connections.TryGetValue(ThreadId, result) then
+	begin
+		if doInit then
+		begin
+			result := TCloudMailRuHTTP.Create(ConnectionSettings, ExternalProgressProc, ExternalLogProc);
+			Connections.AddOrSetValue(ThreadId, result);
+		end;
+	end;
+end;
+
+end.
