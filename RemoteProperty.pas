4d9742ebc5b5ee064560c2964e627c5a6094c1b5
Pozitronik <Pozitronik>
2019-05-28 11:16:23 +0300

    todo added

--- RemoteProperty.pas
+++ RemoteProperty.pas
@@ -340,7 +340,7 @@ procedure TPropertyForm.TempPublicCloudInit(publicUrl: WideString);
 var
 	TempCloudSettings: TCloudSettings;
 begin
-	TempCloudSettings.AccountSettings.public_account := true;
+	TempCloudSettings.AccountSettings.public_account := true;  {TODO: check parameters}
 	TempCloudSettings.AccountSettings.public_url := publicUrl;
 	self.TempPublicCloud := TCloudMailRu.Create(TempCloudSettings, nil);
 	self.TempPublicCloud.login;
