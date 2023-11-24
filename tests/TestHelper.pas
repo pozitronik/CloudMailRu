46d80d95c833f7c968aa0143ea743ae848641066
Pozitronik <antikillerxxl@gmail.com>
2023-11-24 17:11:40 +0400

    introduce tests helper unit

---
+++ tests/TestHelper.pas
@@ -0,0 +1,22 @@
+﻿unit TestHelper;
+
+interface
+
+function RandomString(const Len: Integer): WideString;
+
+implementation
+
+function RandomString(const Len: Integer): WideString;
+const
+	CharSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+var
+	i: Integer;
+begin
+	Result := '';
+	Randomize; // Initialize the random number generator
+
+	for i := 1 to Len do
+		Result := Result + CharSet[Random(Length(CharSet)) + 1];
+end;
+
+end.
