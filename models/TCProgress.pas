23e54e69bcabfa004d0f032cfa29b543e542a5cb
Pozitronik <antikillerxxl@gmail.com>
2023-11-24 17:26:02 +0400

    Introduce TCProgress class middleware

---
+++ models/TCProgress.pas
@@ -0,0 +1,44 @@
+﻿unit TCProgress;
+
+interface
+
+uses
+	SysUtils,
+	PLUGIN_TYPES,
+	SystemHelper;
+
+type
+	TTCProgress = class
+	private
+		PluginNum: Integer;
+		ProgressProc: TProgressProcW;
+	public
+		constructor Create(); overload; //creates a dummy progress
+		constructor Create(ProgressProc: TProgressProcW; PluginNum: Integer); overload;
+		function Progress(SourceName, TargetName: WideString; PercentDone: Integer): Integer; //todo: check if result can be boolean
+	end;
+
+implementation
+
+{TTCProgress}
+
+constructor TTCProgress.Create;
+begin
+	self.ProgressProc := nil;
+	self.PluginNum := -1;
+end;
+
+constructor TTCProgress.Create(ProgressProc: TProgressProcW; PluginNum: Integer);
+begin
+	self.ProgressProc := ProgressProc;
+	self.PluginNum := PluginNum;
+end;
+
+function TTCProgress.Progress(SourceName, TargetName: WideString; PercentDone: Integer): Integer;
+begin
+	Result := -1;
+	if Assigned(ProgressProc) then
+		Result := ProgressProc(PluginNum, PWideChar(SourceName), PWideChar(TargetName), PercentDone);
+end;
+
+end.
