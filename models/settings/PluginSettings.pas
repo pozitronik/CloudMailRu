108705b95481fba7b1b7b3296b99c4f0194f1cb3
Pozitronik <antikillerxxl@gmail.com>
2023-11-26 15:06:17 +0400

    Move TPluginSettings into the separate unit

---
+++ models/settings/PluginSettings.pas
@@ -0,0 +1,45 @@
+﻿unit PluginSettings;
+
+interface
+
+uses
+	ConnectionSettings;
+
+type
+	{Global plugin options}
+	TPluginSettings = record
+		ConnectionSettings: TConnectionSettings;
+		IniPath: integer;
+		LoadSSLDLLOnlyFromPluginDir: boolean;
+		PreserveFileTime: boolean;
+		DescriptionEnabled: boolean;
+		DescriptionEditorEnabled: boolean;
+		DescriptionCopyToCloud: boolean;
+		DescriptionCopyFromCloud: boolean;
+		DescriptionTrackCloudFS: boolean;
+		DescriptionFileName: WideString;
+		CopyBetweenAccountsMode: integer;
+		CloudMaxFileSize: int64;
+		ChunkOverwriteMode: integer;
+		DeleteFailOnUploadMode: integer;
+		OperationErrorMode: integer;
+		RetryAttempts: integer;
+		AttemptWait: integer;
+		OverwriteLocalMode: integer;
+		DisableMultiThreading: boolean;
+		LogUserSpace: boolean;
+		IconsMode: integer;
+		DownloadLinksEncode: boolean;
+		AutoUpdateDownloadListing: boolean;
+		ShowTrashFolders: boolean;
+		ShowSharedFolders: boolean;
+		ShowInvitesFolders: boolean;
+		LogLevel: integer;
+		PrecalculateHash: boolean;
+		ForcePrecalculateSize: int64;
+		CheckCRC: boolean;
+	end;
+
+implementation
+
+end.
