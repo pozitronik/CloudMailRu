1804793441881098154350f48c7d5a715e984e08
Pozitronik <antikillerxxl@gmail.com>
2023-11-28 12:11:36 +0400

    Add stub to test TAcountSettings class

---
+++ tests/models/settings/AccountSettingsTest.pas
@@ -0,0 +1,39 @@
+﻿unit AccountSettingsTest;
+
+interface
+
+uses
+	NewAccountSettings,
+	DUnitX.TestFramework;
+
+type
+
+	[TestFixture]
+	TAccountSettingsTest = class
+	public
+		// Sample Methods
+		// Simple single Test
+		[Test]
+		procedure Test1;
+		// Test with TestCase Attribute to supply parameters.
+		[Test]
+		[TestCase('TestA', '1,2')]
+		[TestCase('TestB', '3,4')]
+		procedure Test2(const AValue1: Integer; const AValue2: Integer);
+	end;
+
+implementation
+
+procedure TAccountSettingsTest.Test1;
+begin
+end;
+
+procedure TAccountSettingsTest.Test2(const AValue1: Integer; const AValue2: Integer);
+begin
+end;
+
+initialization
+
+TDUnitX.RegisterTestFixture(TAccountSettingsTest);
+
+end.
