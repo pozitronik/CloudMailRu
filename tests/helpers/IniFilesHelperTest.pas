e1d2c6a1f832460437e1c7d8cb6fbc823de5cd8e
Pozitronik <antikillerxxl@gmail.com>
2026-01-15 22:10:11 +0400

    Added: tests for WSList, CMROperationResult, CMRSpace, JSONHelper, IniFilesHelper (+65 tests)

---
+++ tests/helpers/IniFilesHelperTest.pas
@@ -0,0 +1,215 @@
+unit IniFilesHelperTest;
+
+interface
+
+uses
+	IniFilesHelper,
+	IniFiles,
+	SysUtils,
+	IOUtils,
+	DUnitX.TestFramework;
+
+type
+
+	[TestFixture]
+	TIniFilesHelperTest = class
+	private
+		FTestIniPath: string;
+		FIniFile: TIniFile;
+	public
+		[Setup]
+		procedure Setup;
+		[TearDown]
+		procedure TearDown;
+		[Test]
+		procedure TestValidateSectionNameValid;
+		[Test]
+		procedure TestValidateSectionNameWithBrackets;
+		[Test]
+		procedure TestValidateSectionNameWithNewline;
+		[Test]
+		procedure TestValidateIdentNameValid;
+		[Test]
+		procedure TestValidateIdentNameStartsWithLetter;
+		[Test]
+		procedure TestValidateIdentNameStartsWithNumber;
+		[Test]
+		procedure TestValidateIdentNameEmpty;
+		[Test]
+		procedure TestReadInt64Valid;
+		[Test]
+		procedure TestReadInt64Hex;
+		[Test]
+		procedure TestReadInt64Default;
+		[Test]
+		procedure TestWriteInt64;
+		[Test]
+		procedure TestWriteIntegerIfNotDefaultWritesValue;
+		[Test]
+		procedure TestWriteIntegerIfNotDefaultDeletesKey;
+		[Test]
+		procedure TestWriteStringIfNotDefaultWritesValue;
+		[Test]
+		procedure TestWriteStringIfNotDefaultDeletesKey;
+		[Test]
+		procedure TestWriteStringInvalidSectionRaises;
+		[Test]
+		procedure TestWriteStringInvalidIdentRaises;
+	end;
+
+implementation
+
+procedure TIniFilesHelperTest.Setup;
+begin
+	FTestIniPath := TPath.Combine(TPath.GetTempPath, 'test_ini_helper_' + IntToStr(Random(100000)) + '.ini');
+	FIniFile := TIniFile.Create(FTestIniPath);
+end;
+
+procedure TIniFilesHelperTest.TearDown;
+begin
+	FIniFile.Free;
+	if TFile.Exists(FTestIniPath) then
+		TFile.Delete(FTestIniPath);
+end;
+
+procedure TIniFilesHelperTest.TestValidateSectionNameValid;
+begin
+	Assert.IsTrue(FIniFile.ValidateSectionName('ValidSection'));
+	Assert.IsTrue(FIniFile.ValidateSectionName('Section With Spaces'));
+	Assert.IsTrue(FIniFile.ValidateSectionName('Section123'));
+end;
+
+procedure TIniFilesHelperTest.TestValidateSectionNameWithBrackets;
+begin
+	{ Section names cannot contain [ or ] }
+	Assert.IsFalse(FIniFile.ValidateSectionName('Invalid[Section'));
+	Assert.IsFalse(FIniFile.ValidateSectionName('Invalid]Section'));
+	Assert.IsFalse(FIniFile.ValidateSectionName('[Invalid]'));
+end;
+
+procedure TIniFilesHelperTest.TestValidateSectionNameWithNewline;
+begin
+	{ Section names cannot contain newlines }
+	Assert.IsFalse(FIniFile.ValidateSectionName('Invalid' + #10 + 'Section'));
+end;
+
+procedure TIniFilesHelperTest.TestValidateIdentNameValid;
+begin
+	Assert.IsTrue(FIniFile.ValidateIdentName('ValidIdent'));
+	Assert.IsTrue(FIniFile.ValidateIdentName('valid_ident'));
+	Assert.IsTrue(FIniFile.ValidateIdentName('Ident123'));
+	Assert.IsTrue(FIniFile.ValidateIdentName('$special'));
+	Assert.IsTrue(FIniFile.ValidateIdentName('.dotstart'));
+end;
+
+procedure TIniFilesHelperTest.TestValidateIdentNameStartsWithLetter;
+begin
+	Assert.IsTrue(FIniFile.ValidateIdentName('aIdent'));
+	Assert.IsTrue(FIniFile.ValidateIdentName('ZIdent'));
+end;
+
+procedure TIniFilesHelperTest.TestValidateIdentNameStartsWithNumber;
+begin
+	{ Identifiers cannot start with a number }
+	Assert.IsFalse(FIniFile.ValidateIdentName('1InvalidIdent'));
+	Assert.IsFalse(FIniFile.ValidateIdentName('9test'));
+end;
+
+procedure TIniFilesHelperTest.TestValidateIdentNameEmpty;
+begin
+	Assert.IsFalse(FIniFile.ValidateIdentName(''));
+end;
+
+procedure TIniFilesHelperTest.TestReadInt64Valid;
+begin
+	FIniFile.WriteString('TestSection', 'Int64Value', '9223372036854775807');
+
+	Assert.AreEqual(Int64(9223372036854775807), FIniFile.ReadInt64('TestSection', 'Int64Value', 0));
+end;
+
+procedure TIniFilesHelperTest.TestReadInt64Hex;
+begin
+	{ ReadInt64 supports 0x hex notation }
+	FIniFile.WriteString('TestSection', 'HexValue', '0xFF');
+
+	Assert.AreEqual(Int64(255), FIniFile.ReadInt64('TestSection', 'HexValue', 0));
+end;
+
+procedure TIniFilesHelperTest.TestReadInt64Default;
+begin
+	Assert.AreEqual(Int64(42), FIniFile.ReadInt64('TestSection', 'NonExistent', 42));
+end;
+
+procedure TIniFilesHelperTest.TestWriteInt64;
+begin
+	FIniFile.WriteInt64('TestSection', 'TestKey', 123456789012345);
+
+	Assert.AreEqual(Int64(123456789012345), FIniFile.ReadInt64('TestSection', 'TestKey', 0));
+end;
+
+procedure TIniFilesHelperTest.TestWriteIntegerIfNotDefaultWritesValue;
+begin
+	FIniFile.WriteIntegerIfNotDefault('TestSection', 'TestKey', 100, 0);
+
+	Assert.AreEqual(100, FIniFile.ReadInteger('TestSection', 'TestKey', 0));
+end;
+
+procedure TIniFilesHelperTest.TestWriteIntegerIfNotDefaultDeletesKey;
+begin
+	{ First write a value }
+	FIniFile.WriteInteger('TestSection', 'TestKey', 100);
+	Assert.AreEqual(100, FIniFile.ReadInteger('TestSection', 'TestKey', 0));
+
+	{ Now write default - should delete }
+	FIniFile.WriteIntegerIfNotDefault('TestSection', 'TestKey', 0, 0);
+
+	{ Key should be gone, return default }
+	Assert.AreEqual(999, FIniFile.ReadInteger('TestSection', 'TestKey', 999));
+end;
+
+procedure TIniFilesHelperTest.TestWriteStringIfNotDefaultWritesValue;
+begin
+	FIniFile.WriteStringIfNotDefault('TestSection', 'TestKey', 'custom', 'default');
+
+	Assert.AreEqual('custom', FIniFile.ReadString('TestSection', 'TestKey', ''));
+end;
+
+procedure TIniFilesHelperTest.TestWriteStringIfNotDefaultDeletesKey;
+begin
+	{ First write a value }
+	FIniFile.WriteString('TestSection', 'TestKey', 'custom');
+
+	{ Now write default - should delete }
+	FIniFile.WriteStringIfNotDefault('TestSection', 'TestKey', 'default', 'default');
+
+	{ Key should be gone }
+	Assert.AreEqual('fallback', FIniFile.ReadString('TestSection', 'TestKey', 'fallback'));
+end;
+
+procedure TIniFilesHelperTest.TestWriteStringInvalidSectionRaises;
+begin
+	Assert.WillRaise(
+		procedure
+		begin
+			FIniFile.WriteString('Invalid[Section', 'key', 'value');
+		end,
+		EIniFileException
+	);
+end;
+
+procedure TIniFilesHelperTest.TestWriteStringInvalidIdentRaises;
+begin
+	Assert.WillRaise(
+		procedure
+		begin
+			FIniFile.WriteString('Section', '1invalid', 'value');
+		end,
+		EIniFileException
+	);
+end;
+
+initialization
+
+TDUnitX.RegisterTestFixture(TIniFilesHelperTest);
+
+end.
---
+++ tests/helpers/JSONHelperTest.pas
@@ -0,0 +1,213 @@
+unit JSONHelperTest;
+
+interface
+
+uses
+	JSONHelper,
+	CMRConstants,
+	JSON,
+	DUnitX.TestFramework;
+
+type
+
+	[TestFixture]
+	TJSONHelperTest = class
+	public
+		[Test]
+		procedure TestInitValid;
+		[Test]
+		procedure TestInitInvalid;
+		[Test]
+		procedure TestInitEmpty;
+		[Test]
+		procedure TestGetPublicLinkValid;
+		[Test]
+		procedure TestGetPublicLinkInvalid;
+		[Test]
+		procedure TestGetShardValid;
+		[Test]
+		procedure TestGetShardInvalid;
+		[Test]
+		procedure TestGetBodyErrorValid;
+		[Test]
+		procedure TestGetBodyErrorInvalid;
+		[Test]
+		procedure TestGetBodyTokenValid;
+		[Test]
+		procedure TestGetBodyTokenInvalid;
+		[Test]
+		procedure TestAssignFromNameWideString;
+		[Test]
+		procedure TestAssignFromNameInt64;
+		[Test]
+		procedure TestAssignFromNameInteger;
+		[Test]
+		procedure TestAssignFromNameBoolean;
+		[Test]
+		procedure TestAssignFromNameMissing;
+	end;
+
+implementation
+
+const
+	JSON_PUBLIC_LINK = '{"status":200,"body":"/ABC123/shared_file"}';
+	JSON_SHARD = '{"status":200,"body":{"get":[{"url":"https://cloclo1.cloud.mail.ru/get/"}]}}';
+	JSON_BODY_ERROR = '{"status":400,"body":"token"}';
+	JSON_BODY_TOKEN = '{"status":200,"body":{"token":"csrf_token_value"}}';
+	JSON_TEST_OBJECT = '{"name":"test","size":12345,"count":42,"active":true}';
+
+procedure TJSONHelperTest.TestInitValid;
+var
+	JSONVal: TJSONObject;
+begin
+	Assert.IsTrue(init('{"key":"value"}', JSONVal));
+	Assert.IsNotNull(JSONVal);
+	JSONVal.Free;
+end;
+
+procedure TJSONHelperTest.TestInitInvalid;
+var
+	JSONVal: TJSONObject;
+begin
+	Assert.IsFalse(init('not valid json', JSONVal));
+end;
+
+procedure TJSONHelperTest.TestInitEmpty;
+var
+	JSONVal: TJSONObject;
+begin
+	Assert.IsFalse(init('', JSONVal));
+end;
+
+procedure TJSONHelperTest.TestGetPublicLinkValid;
+var
+	Link: WideString;
+begin
+	Assert.IsTrue(getPublicLink(JSON_PUBLIC_LINK, Link));
+	Assert.AreEqual('/ABC123/shared_file', Link);
+end;
+
+procedure TJSONHelperTest.TestGetPublicLinkInvalid;
+var
+	Link: WideString;
+begin
+	Assert.IsFalse(getPublicLink('invalid', Link));
+end;
+
+procedure TJSONHelperTest.TestGetShardValid;
+var
+	Shard: WideString;
+begin
+	Assert.IsTrue(getShard(JSON_SHARD, Shard, SHARD_TYPE_GET));
+	Assert.AreEqual('https://cloclo1.cloud.mail.ru/get/', Shard);
+end;
+
+procedure TJSONHelperTest.TestGetShardInvalid;
+var
+	Shard: WideString;
+begin
+	Assert.IsFalse(getShard('invalid', Shard));
+end;
+
+procedure TJSONHelperTest.TestGetBodyErrorValid;
+var
+	Error: WideString;
+begin
+	Error := getBodyError(JSON_BODY_ERROR);
+	Assert.AreEqual('token', Error);
+end;
+
+procedure TJSONHelperTest.TestGetBodyErrorInvalid;
+var
+	Error: WideString;
+begin
+	Error := getBodyError('invalid');
+	Assert.AreEqual('', Error);
+end;
+
+procedure TJSONHelperTest.TestGetBodyTokenValid;
+var
+	Token: WideString;
+begin
+	Assert.IsTrue(getBodyToken(JSON_BODY_TOKEN, Token));
+	Assert.AreEqual('csrf_token_value', Token);
+end;
+
+procedure TJSONHelperTest.TestGetBodyTokenInvalid;
+var
+	Token: WideString;
+begin
+	Assert.IsFalse(getBodyToken('invalid', Token));
+end;
+
+procedure TJSONHelperTest.TestAssignFromNameWideString;
+var
+	JSONVal: TJSONObject;
+	Value: WideString;
+begin
+	Value := '';
+	init(JSON_TEST_OBJECT, JSONVal);
+	assignFromName('name', JSONVal, Value);
+
+	Assert.AreEqual('test', Value);
+	JSONVal.Free;
+end;
+
+procedure TJSONHelperTest.TestAssignFromNameInt64;
+var
+	JSONVal: TJSONObject;
+	Value: Int64;
+begin
+	Value := 0;
+	init(JSON_TEST_OBJECT, JSONVal);
+	assignFromName('size', JSONVal, Value);
+
+	Assert.AreEqual(Int64(12345), Value);
+	JSONVal.Free;
+end;
+
+procedure TJSONHelperTest.TestAssignFromNameInteger;
+var
+	JSONVal: TJSONObject;
+	Value: Integer;
+begin
+	Value := 0;
+	init(JSON_TEST_OBJECT, JSONVal);
+	assignFromName('count', JSONVal, Value);
+
+	Assert.AreEqual(42, Value);
+	JSONVal.Free;
+end;
+
+procedure TJSONHelperTest.TestAssignFromNameBoolean;
+var
+	JSONVal: TJSONObject;
+	Value: Boolean;
+begin
+	Value := False;
+	init(JSON_TEST_OBJECT, JSONVal);
+	assignFromName('active', JSONVal, Value);
+
+	Assert.IsTrue(Value);
+	JSONVal.Free;
+end;
+
+procedure TJSONHelperTest.TestAssignFromNameMissing;
+var
+	JSONVal: TJSONObject;
+	Value: WideString;
+begin
+	Value := 'default';
+	init(JSON_TEST_OBJECT, JSONVal);
+	assignFromName('nonexistent', JSONVal, Value);
+
+	{ Value should remain unchanged when key is missing }
+	Assert.AreEqual('default', Value);
+	JSONVal.Free;
+end;
+
+initialization
+
+TDUnitX.RegisterTestFixture(TJSONHelperTest);
+
+end.
