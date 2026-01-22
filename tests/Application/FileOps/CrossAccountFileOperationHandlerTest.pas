92f8a2d10d1552e1927cdc0aef0b4738a30ecdde
Pozitronik <antikillerxxl@gmail.com>
2026-01-22 22:33:37 +0400

    Added: comprehensive tests for CrossAccountFileOperationHandler ViaHash and ViaPublicLink modes

--- tests/Application/FileOps/CrossAccountFileOperationHandlerTest.pas
+++ tests/Application/FileOps/CrossAccountFileOperationHandlerTest.pas
@@ -1,7 +1,7 @@
 unit CrossAccountFileOperationHandlerTest;
 
 {Unit tests for TCrossAccountFileOperationHandler.
- Note: Full integration tests require TCloudMailRu which isn't interface-based.}
+ Tests cross-account file operations including ViaHash and ViaPublicLink modes.}
 
 interface
 
@@ -11,7 +11,15 @@ uses
 	CrossAccountFileOperationHandler,
 	RetryHandler,
 	TCLogger,
-	RealPath;
+	TCProgress,
+	TCRequest,
+	RealPath,
+	CloudMailRu,
+	CloudSettings,
+	MockCloudHTTP,
+	MockHTTPManager,
+	IAuthStrategyInterface,
+	WindowsFileSystem;
 
 type
 	{Mock retry handler that tracks calls}
@@ -45,12 +53,24 @@ type
 		property LastMessage: WideString read FLastMessage;
 	end;
 
+	{Testable CloudMailRu for cross-account operation tests}
+	TTestableCloudMailRu = class(TCloudMailRu)
+	public
+		procedure SetUnitedParams(const Value: WideString);
+	end;
+
 	[TestFixture]
 	TCrossAccountFileOperationHandlerTest = class
 	private
 		FHandler: ICrossAccountFileOperationHandler;
 		FMockRetryHandler: TMockRetryHandler;
 		FMockLogger: TMockLogger;
+		FMockHTTP: TMockCloudHTTP;
+		FMockHTTPManager: TMockHTTPManager;
+		FOldCloud: TTestableCloudMailRu;
+		FNewCloud: TTestableCloudMailRu;
+
+		function CreateCloud: TTestableCloudMailRu;
 	public
 		[Setup]
 		procedure Setup;
@@ -73,11 +93,31 @@ type
 		[Test]
 		procedure TestExecute_InvalidMode_ReturnsWriteError;
 
-		{Mode delegation tests - require integration}
+		{ViaHash mode tests}
 		[Test]
-		procedure TestExecute_ViaHash_RequiresIntegration;
+		procedure TestExecute_ViaHash_StatusFileFails_ReturnsNotSupported;
 		[Test]
-		procedure TestExecute_ViaPublicLink_RequiresIntegration;
+		procedure TestExecute_ViaHash_AddByIdentitySucceeds_ReturnsOK;
+		[Test]
+		procedure TestExecute_ViaHash_WithOverwrite_DeletesTargetFirst;
+		[Test]
+		procedure TestExecute_ViaHash_MoveSucceeds_DeletesSource;
+		[Test]
+		procedure TestExecute_ViaHash_AddByIdentityFails_CallsRetryHandler;
+
+		{ViaPublicLink mode tests}
+		[Test]
+		procedure TestExecute_ViaPublicLink_StatusFileFails_ReturnsNotSupported;
+		[Test]
+		procedure TestExecute_ViaPublicLink_AlreadyPublished_ClonesDirectly;
+		[Test]
+		procedure TestExecute_ViaPublicLink_NotPublished_PublishesFirst;
+		[Test]
+		procedure TestExecute_ViaPublicLink_CloneSucceeds_ReturnsOK;
+		[Test]
+		procedure TestExecute_ViaPublicLink_CloneFails_CallsRetryHandler;
+		[Test]
+		procedure TestExecute_ViaPublicLink_TempPublished_UnpublishesAfter;
 	end;
 
 implementation
@@ -87,6 +127,42 @@ uses
 	CMRConstants,
 	SETTINGS_CONSTANTS,
 	LANGUAGE_STRINGS;
+
+const
+	{Sample JSON responses for cross-account operations}
+	JSON_STATUS_FILE_SUCCESS =
+		'{"email":"test@mail.ru","body":{' +
+		'"name":"file.txt","size":1024,"kind":"file","mtime":1234567890,' +
+		'"hash":"1234567890123456789012345678901234567890"' +
+		'},"status":200}';
+
+	JSON_STATUS_FILE_PUBLISHED =
+		'{"email":"test@mail.ru","body":{' +
+		'"name":"file.txt","size":1024,"kind":"file","mtime":1234567890,' +
+		'"hash":"1234567890123456789012345678901234567890",' +
+		'"weblink":"abc123public"' +
+		'},"status":200}';
+
+	JSON_STATUS_FILE_NOT_EXISTS =
+		'{"email":"test@mail.ru","body":{"home":{"error":"not_exists"}},"status":400}';
+
+	JSON_ADD_FILE_SUCCESS =
+		'{"email":"test@mail.ru","body":{"hash":"1234567890123456789012345678901234567890"},"status":200}';
+
+	JSON_ADD_FILE_EXISTS =
+		'{"email":"test@mail.ru","body":{"home":{"error":"exists"}},"status":400}';
+
+	JSON_DELETE_SUCCESS =
+		'{"email":"test@mail.ru","body":"ok","status":200}';
+
+	JSON_PUBLISH_SUCCESS =
+		'{"email":"test@mail.ru","body":"abc123public","status":200}';
+
+	JSON_CLONE_SUCCESS =
+		'{"email":"test@mail.ru","body":"/cloned_file.txt","status":200}';
+
+	JSON_CLONE_FAIL =
+		'{"email":"test@mail.ru","body":{"home":{"error":"not_exists"}},"status":400}';
 
 {TMockRetryHandler}
 
@@ -125,20 +201,51 @@ begin
 	FLastMessage := LogString;
 end;
 
+{TTestableCloudMailRu}
+
+procedure TTestableCloudMailRu.SetUnitedParams(const Value: WideString);
+begin
+	FUnitedParams := Value;
+end;
+
 {TCrossAccountFileOperationHandlerTest}
 
 procedure TCrossAccountFileOperationHandlerTest.Setup;
 begin
+	FMockHTTP := TMockCloudHTTP.Create;
+	FMockHTTPManager := TMockHTTPManager.Create(FMockHTTP);
 	FMockRetryHandler := TMockRetryHandler.Create;
 	FMockLogger := TMockLogger.Create;
 	FHandler := TCrossAccountFileOperationHandler.Create(FMockRetryHandler, FMockLogger);
+	FOldCloud := nil;
+	FNewCloud := nil;
 end;
 
 procedure TCrossAccountFileOperationHandlerTest.TearDown;
 begin
 	FHandler := nil;
+	FOldCloud.Free;
+	FNewCloud.Free;
 	FMockRetryHandler := nil;
 	FMockLogger := nil;
+	FMockHTTPManager := nil;
+	FMockHTTP := nil;
+end;
+
+function TCrossAccountFileOperationHandlerTest.CreateCloud: TTestableCloudMailRu;
+var
+	Settings: TCloudSettings;
+begin
+	Settings := Default(TCloudSettings);
+	Result := TTestableCloudMailRu.Create(
+		Settings,
+		FMockHTTPManager,
+		TNullAuthStrategy.Create,
+		TNullFileSystem.Create,
+		TNullLogger.Create,
+		TNullProgress.Create,
+		TNullRequest.Create);
+	Result.SetUnitedParams('api=2&access_token=test_token');
 end;
 
 procedure TCrossAccountFileOperationHandlerTest.TestExecute_PublicAccount_ReturnsUserAbort;
@@ -219,18 +326,239 @@ begin
 	Assert.AreEqual(FS_FILE_WRITEERROR, Result, 'Should return write error for invalid mode');
 end;
 
-procedure TCrossAccountFileOperationHandlerTest.TestExecute_ViaHash_RequiresIntegration;
+{ViaHash mode tests}
+
+procedure TCrossAccountFileOperationHandlerTest.TestExecute_ViaHash_StatusFileFails_ReturnsNotSupported;
+var
+	OldPath, NewPath: TRealPath;
+	Result: Integer;
 begin
-	{ViaHash mode requires real TCloudMailRu instances to test file identity operations.
-	 The handler calls OldCloud.statusFile and NewCloud.addFileByIdentity.}
-	Assert.Pass('ViaHash mode tested through integration tests');
+	FOldCloud := CreateCloud;
+	FNewCloud := CreateCloud;
+	OldPath.FromPath('\account1\file.txt');
+	NewPath.FromPath('\account2\file.txt');
+
+	{StatusFile fails - file not found}
+	FMockHTTP.SetResponse(API_FILE, False, '');
+
+	Result := FHandler.Execute(FOldCloud, FNewCloud, OldPath, NewPath, False, False,
+		CopyBetweenAccountsModeViaHash, False,
+		function: Boolean begin Result := False; end);
+
+	Assert.AreEqual(FS_FILE_NOTSUPPORTED, Result, 'Should return not supported when statusFile fails');
 end;
 
-procedure TCrossAccountFileOperationHandlerTest.TestExecute_ViaPublicLink_RequiresIntegration;
+procedure TCrossAccountFileOperationHandlerTest.TestExecute_ViaHash_AddByIdentitySucceeds_ReturnsOK;
+var
+	OldPath, NewPath: TRealPath;
+	Result: Integer;
 begin
-	{ViaPublicLink mode requires real TCloudMailRu instances to test weblink operations.
-	 The handler calls publishFile, CloneWeblink, and optionally unpublishFile.}
-	Assert.Pass('ViaPublicLink mode tested through integration tests');
+	FOldCloud := CreateCloud;
+	FNewCloud := CreateCloud;
+	OldPath.FromPath('\account1\file.txt');
+	NewPath.FromPath('\account2\file.txt');
+
+	{StatusFile succeeds, AddFileByIdentity succeeds}
+	FMockHTTP.SetResponse(API_FILE, True, JSON_STATUS_FILE_SUCCESS);
+	FMockHTTP.SetResponse(API_FILE_ADD, True, JSON_ADD_FILE_SUCCESS);
+
+	Result := FHandler.Execute(FOldCloud, FNewCloud, OldPath, NewPath, False, False,
+		CopyBetweenAccountsModeViaHash, False,
+		function: Boolean begin Result := False; end);
+
+	Assert.AreEqual(CLOUD_OPERATION_OK, Result, 'Should return OK when addByIdentity succeeds');
+end;
+
+procedure TCrossAccountFileOperationHandlerTest.TestExecute_ViaHash_WithOverwrite_DeletesTargetFirst;
+var
+	OldPath, NewPath: TRealPath;
+begin
+	FOldCloud := CreateCloud;
+	FNewCloud := CreateCloud;
+	OldPath.FromPath('\account1\file.txt');
+	NewPath.FromPath('\account2\file.txt');
+
+	{Delete succeeds, StatusFile succeeds, AddFileByIdentity succeeds}
+	FMockHTTP.SetResponse(API_FILE_REMOVE, True, JSON_DELETE_SUCCESS);
+	FMockHTTP.SetResponse(API_FILE, True, JSON_STATUS_FILE_SUCCESS);
+	FMockHTTP.SetResponse(API_FILE_ADD, True, JSON_ADD_FILE_SUCCESS);
+
+	FHandler.Execute(FOldCloud, FNewCloud, OldPath, NewPath, False, True, {OverWrite=True}
+		CopyBetweenAccountsModeViaHash, False,
+		function: Boolean begin Result := False; end);
+
+	Assert.IsTrue(FMockHTTP.WasURLCalled(API_FILE_REMOVE), 'Should call delete API when overwrite is true');
+end;
+
+procedure TCrossAccountFileOperationHandlerTest.TestExecute_ViaHash_MoveSucceeds_DeletesSource;
+var
+	OldPath, NewPath: TRealPath;
+begin
+	FOldCloud := CreateCloud;
+	FNewCloud := CreateCloud;
+	OldPath.FromPath('\account1\file.txt');
+	NewPath.FromPath('\account2\file.txt');
+
+	{StatusFile succeeds, AddFileByIdentity succeeds, Delete source succeeds}
+	FMockHTTP.SetResponse(API_FILE, True, JSON_STATUS_FILE_SUCCESS);
+	FMockHTTP.SetResponse(API_FILE_ADD, True, JSON_ADD_FILE_SUCCESS);
+	FMockHTTP.SetResponse(API_FILE_REMOVE, True, JSON_DELETE_SUCCESS);
+
+	FHandler.Execute(FOldCloud, FNewCloud, OldPath, NewPath, True, False, {Move=True}
+		CopyBetweenAccountsModeViaHash, False,
+		function: Boolean begin Result := False; end);
+
+	{Delete should be called to remove source after successful copy}
+	Assert.IsTrue(FMockHTTP.WasURLCalled(API_FILE_REMOVE), 'Should delete source after successful move');
+end;
+
+procedure TCrossAccountFileOperationHandlerTest.TestExecute_ViaHash_AddByIdentityFails_CallsRetryHandler;
+var
+	OldPath, NewPath: TRealPath;
+begin
+	FOldCloud := CreateCloud;
+	FNewCloud := CreateCloud;
+	OldPath.FromPath('\account1\file.txt');
+	NewPath.FromPath('\account2\file.txt');
+
+	{StatusFile succeeds, but AddFileByIdentity fails with error}
+	FMockHTTP.SetResponse(API_FILE, True, JSON_STATUS_FILE_SUCCESS);
+	FMockHTTP.SetResponse(API_FILE_ADD, False, '');
+
+	FHandler.Execute(FOldCloud, FNewCloud, OldPath, NewPath, False, False,
+		CopyBetweenAccountsModeViaHash, False,
+		function: Boolean begin Result := False; end);
+
+	Assert.IsTrue(FMockRetryHandler.HandleErrorCalled, 'Should call retry handler when addByIdentity fails');
+end;
+
+{ViaPublicLink mode tests}
+
+procedure TCrossAccountFileOperationHandlerTest.TestExecute_ViaPublicLink_StatusFileFails_ReturnsNotSupported;
+var
+	OldPath, NewPath: TRealPath;
+	Result: Integer;
+begin
+	FOldCloud := CreateCloud;
+	FNewCloud := CreateCloud;
+	OldPath.FromPath('\account1\file.txt');
+	NewPath.FromPath('\account2\file.txt');
+
+	{StatusFile fails}
+	FMockHTTP.SetResponse(API_FILE, False, '');
+
+	Result := FHandler.Execute(FOldCloud, FNewCloud, OldPath, NewPath, False, False,
+		CopyBetweenAccountsModeViaPublicLink, False,
+		function: Boolean begin Result := False; end);
+
+	Assert.AreEqual(FS_FILE_NOTSUPPORTED, Result, 'Should return not supported when statusFile fails');
+end;
+
+procedure TCrossAccountFileOperationHandlerTest.TestExecute_ViaPublicLink_AlreadyPublished_ClonesDirectly;
+var
+	OldPath, NewPath: TRealPath;
+begin
+	FOldCloud := CreateCloud;
+	FNewCloud := CreateCloud;
+	OldPath.FromPath('\account1\file.txt');
+	NewPath.FromPath('\account2\file.txt');
+
+	{File is already published - should not call publish API}
+	FMockHTTP.SetResponse(API_FILE, True, JSON_STATUS_FILE_PUBLISHED);
+	FMockHTTP.SetResponse(API_CLONE, True, JSON_CLONE_SUCCESS);
+
+	FHandler.Execute(FOldCloud, FNewCloud, OldPath, NewPath, False, False,
+		CopyBetweenAccountsModeViaPublicLink, False,
+		function: Boolean begin Result := False; end);
+
+	Assert.IsTrue(FMockHTTP.WasURLCalled(API_CLONE), 'Should call clone API');
+	Assert.IsFalse(FMockHTTP.WasURLCalled(API_FILE_PUBLISH), 'Should not call publish API for already published file');
+end;
+
+procedure TCrossAccountFileOperationHandlerTest.TestExecute_ViaPublicLink_NotPublished_PublishesFirst;
+var
+	OldPath, NewPath: TRealPath;
+begin
+	FOldCloud := CreateCloud;
+	FNewCloud := CreateCloud;
+	OldPath.FromPath('\account1\file.txt');
+	NewPath.FromPath('\account2\file.txt');
+
+	{File is not published - should call publish API first}
+	FMockHTTP.SetResponse(API_FILE, True, JSON_STATUS_FILE_SUCCESS);
+	FMockHTTP.SetResponse(API_FILE_PUBLISH, True, JSON_PUBLISH_SUCCESS);
+	FMockHTTP.SetResponse(API_CLONE, True, JSON_CLONE_SUCCESS);
+	FMockHTTP.SetResponse(API_FILE_UNPUBLISH, True, JSON_DELETE_SUCCESS);
+
+	FHandler.Execute(FOldCloud, FNewCloud, OldPath, NewPath, False, False,
+		CopyBetweenAccountsModeViaPublicLink, False,
+		function: Boolean begin Result := False; end);
+
+	Assert.IsTrue(FMockHTTP.WasURLCalled(API_FILE_PUBLISH), 'Should call publish API for unpublished file');
+	Assert.IsTrue(FMockHTTP.WasURLCalled(API_CLONE), 'Should call clone API');
+end;
+
+procedure TCrossAccountFileOperationHandlerTest.TestExecute_ViaPublicLink_CloneSucceeds_ReturnsOK;
+var
+	OldPath, NewPath: TRealPath;
+	Result: Integer;
+begin
+	FOldCloud := CreateCloud;
+	FNewCloud := CreateCloud;
+	OldPath.FromPath('\account1\file.txt');
+	NewPath.FromPath('\account2\file.txt');
+
+	{Clone operation succeeds}
+	FMockHTTP.SetResponse(API_FILE, True, JSON_STATUS_FILE_PUBLISHED);
+	FMockHTTP.SetResponse(API_CLONE, True, JSON_CLONE_SUCCESS);
+
+	Result := FHandler.Execute(FOldCloud, FNewCloud, OldPath, NewPath, False, False,
+		CopyBetweenAccountsModeViaPublicLink, False,
+		function: Boolean begin Result := False; end);
+
+	Assert.AreEqual(CLOUD_OPERATION_OK, Result, 'Should return OK when clone succeeds');
+end;
+
+procedure TCrossAccountFileOperationHandlerTest.TestExecute_ViaPublicLink_CloneFails_CallsRetryHandler;
+var
+	OldPath, NewPath: TRealPath;
+begin
+	FOldCloud := CreateCloud;
+	FNewCloud := CreateCloud;
+	OldPath.FromPath('\account1\file.txt');
+	NewPath.FromPath('\account2\file.txt');
+
+	{Clone operation fails}
+	FMockHTTP.SetResponse(API_FILE, True, JSON_STATUS_FILE_PUBLISHED);
+	FMockHTTP.SetResponse(API_CLONE, False, '');
+
+	FHandler.Execute(FOldCloud, FNewCloud, OldPath, NewPath, False, False,
+		CopyBetweenAccountsModeViaPublicLink, False,
+		function: Boolean begin Result := False; end);
+
+	Assert.IsTrue(FMockRetryHandler.HandleErrorCalled, 'Should call retry handler when clone fails');
+end;
+
+procedure TCrossAccountFileOperationHandlerTest.TestExecute_ViaPublicLink_TempPublished_UnpublishesAfter;
+var
+	OldPath, NewPath: TRealPath;
+begin
+	FOldCloud := CreateCloud;
+	FNewCloud := CreateCloud;
+	OldPath.FromPath('\account1\file.txt');
+	NewPath.FromPath('\account2\file.txt');
+
+	{File was not published, so temp link should be removed after clone}
+	FMockHTTP.SetResponse(API_FILE, True, JSON_STATUS_FILE_SUCCESS);
+	FMockHTTP.SetResponse(API_FILE_PUBLISH, True, JSON_PUBLISH_SUCCESS);
+	FMockHTTP.SetResponse(API_CLONE, True, JSON_CLONE_SUCCESS);
+	FMockHTTP.SetResponse(API_FILE_UNPUBLISH, True, JSON_DELETE_SUCCESS);
+
+	FHandler.Execute(FOldCloud, FNewCloud, OldPath, NewPath, False, False,
+		CopyBetweenAccountsModeViaPublicLink, False,
+		function: Boolean begin Result := False; end);
+
+	Assert.IsTrue(FMockHTTP.WasURLCalled(API_FILE_UNPUBLISH), 'Should call unpublish API to remove temp link');
 end;
 
 initialization
--- tests/Application/Listing/ListingProviderTest.pas
+++ tests/Application/Listing/ListingProviderTest.pas
@@ -9,16 +9,38 @@ uses
 	System.SysUtils,
 	DUnitX.TestFramework,
 	ListingProvider,
+	CloudMailRu,
+	CloudSettings,
+	MockCloudHTTP,
+	MockHTTPManager,
+	IAuthStrategyInterface,
+	WindowsFileSystem,
+	TCLogger,
+	TCProgress,
+	TCRequest,
 	RealPath,
+	CMRDirItem,
 	CMRDirItemList,
 	CMRIncomingInviteList,
+	CMRConstants,
 	SETTINGS_CONSTANTS;
 
 type
+	{Testable CloudMailRu for listing tests}
+	TTestableCloudMailRu = class(TCloudMailRu)
+	public
+		procedure SetUnitedParams(const Value: WideString);
+	end;
+
 	[TestFixture]
 	TListingProviderTest = class
 	private
+		FMockHTTP: TMockCloudHTTP;
+		FMockHTTPManager: TMockHTTPManager;
+		FCloud: TTestableCloudMailRu;
 		FProvider: IListingProvider;
+
+		function CreateCloud: TTestableCloudMailRu;
 	public
 		[Setup]
 		procedure Setup;
@@ -40,18 +62,100 @@ type
 		procedure TestCreate_ReturnsValidInstance;
 		[Test]
 		procedure TestCreate_ImplementsInterface;
+
+		{FetchListing tests - verify correct cloud API is called}
+		[Test]
+		procedure TestFetchListing_TrashPath_CallsTrashbinAPI;
+		[Test]
+		procedure TestFetchListing_SharedPath_CallsSharedLinksAPI;
+		[Test]
+		procedure TestFetchListing_InvitesPath_CallsIncomingLinksAPI;
+		[Test]
+		procedure TestFetchListing_NormalPath_CallsDirListingAPI;
+		[Test]
+		procedure TestFetchListing_TrashPath_ReturnsSuccess;
+		[Test]
+		procedure TestFetchListing_SharedPath_ReturnsSuccess;
+		[Test]
+		procedure TestFetchListing_InvitesPath_ReturnsSuccess;
+		[Test]
+		procedure TestFetchListing_NormalPath_ReturnsSuccess;
+		[Test]
+		procedure TestFetchListing_APIFailure_ReturnsFalse;
+		[Test]
+		procedure TestFetchListing_PopulatesListing;
 	end;
 
 implementation
 
+const
+	{Sample JSON responses for different listing types}
+	JSON_TRASHBIN_LISTING =
+		'{"email":"test@mail.ru","body":{"list":[' +
+		'{"name":"deleted.txt","size":100,"kind":"file","mtime":1234567890}' +
+		']},"status":200}';
+
+	JSON_SHARED_LINKS_LISTING =
+		'{"email":"test@mail.ru","body":{"list":[' +
+		'{"name":"shared.txt","size":200,"kind":"file","weblink":"abc123"}' +
+		']},"status":200}';
+
+	JSON_INCOMING_INVITES_LISTING =
+		'{"email":"test@mail.ru","body":{"list":[' +
+		'{"name":"invite_folder","tree":"shared_tree","access":"read_only","invite_token":"token123",' +
+		'"owner":{"email":"owner@mail.ru","name":"Owner"}}' +
+		']},"status":200}';
+
+	JSON_DIR_LISTING =
+		'{"email":"test@mail.ru","body":{"list":[' +
+		'{"name":"file.txt","size":300,"kind":"file","mtime":1234567890},' +
+		'{"name":"subfolder","kind":"folder","mtime":1234567890}' +
+		']},"status":200}';
+
+	JSON_EMPTY_LISTING =
+		'{"email":"test@mail.ru","body":{"list":[]},"status":200}';
+
+	JSON_FAILURE =
+		'{"email":"test@mail.ru","body":{"home":{"error":"not_exists"}},"status":400}';
+
+{TTestableCloudMailRu}
+
+procedure TTestableCloudMailRu.SetUnitedParams(const Value: WideString);
+begin
+	FUnitedParams := Value;
+end;
+
+{TListingProviderTest}
+
 procedure TListingProviderTest.Setup;
 begin
+	FMockHTTP := TMockCloudHTTP.Create;
+	FMockHTTPManager := TMockHTTPManager.Create(FMockHTTP);
 	FProvider := TListingProvider.Create;
 end;
 
 procedure TListingProviderTest.TearDown;
 begin
 	FProvider := nil;
+	FCloud.Free;
+	FMockHTTPManager := nil;
+	FMockHTTP := nil;
+end;
+
+function TListingProviderTest.CreateCloud: TTestableCloudMailRu;
+var
+	Settings: TCloudSettings;
+begin
+	Settings := Default(TCloudSettings);
+	Result := TTestableCloudMailRu.Create(
+		Settings,
+		FMockHTTPManager,
+		TNullAuthStrategy.Create,
+		TNullFileSystem.Create,
+		TNullLogger.Create,
+		TNullProgress.Create,
+		TNullRequest.Create);
+	Result.SetUnitedParams('api=2&access_token=test_token');
 end;
 
 {Path type detection tests}
@@ -109,6 +213,165 @@ var
 begin
 	Assert.IsTrue(Supports(TListingProvider.Create, IListingProvider, Intf),
 		'TListingProvider should implement IListingProvider');
+end;
+
+{FetchListing tests}
+
+procedure TListingProviderTest.TestFetchListing_TrashPath_CallsTrashbinAPI;
+var
+	Path: TRealPath;
+	DirListing: TCMRDirItemList;
+	InviteListing: TCMRIncomingInviteList;
+begin
+	FCloud := CreateCloud;
+	FMockHTTP.SetResponse(API_TRASHBIN, True, JSON_TRASHBIN_LISTING);
+	Path.FromPath('\account' + TrashPostfix);
+
+	FProvider.FetchListing(FCloud, Path, DirListing, InviteListing);
+
+	Assert.IsTrue(FMockHTTP.WasURLCalled(API_TRASHBIN), 'Should call trashbin API');
+end;
+
+procedure TListingProviderTest.TestFetchListing_SharedPath_CallsSharedLinksAPI;
+var
+	Path: TRealPath;
+	DirListing: TCMRDirItemList;
+	InviteListing: TCMRIncomingInviteList;
+begin
+	FCloud := CreateCloud;
+	FMockHTTP.SetResponse(API_FOLDER_SHARED_LINKS, True, JSON_SHARED_LINKS_LISTING);
+	Path.FromPath('\account' + SharedPostfix);
+
+	FProvider.FetchListing(FCloud, Path, DirListing, InviteListing);
+
+	Assert.IsTrue(FMockHTTP.WasURLCalled(API_FOLDER_SHARED_LINKS), 'Should call shared links API');
+end;
+
+procedure TListingProviderTest.TestFetchListing_InvitesPath_CallsIncomingLinksAPI;
+var
+	Path: TRealPath;
+	DirListing: TCMRDirItemList;
+	InviteListing: TCMRIncomingInviteList;
+begin
+	FCloud := CreateCloud;
+	FMockHTTP.SetResponse(API_FOLDER_SHARED_INCOMING, True, JSON_INCOMING_INVITES_LISTING);
+	Path.FromPath('\account' + InvitesPostfix);
+
+	FProvider.FetchListing(FCloud, Path, DirListing, InviteListing);
+
+	Assert.IsTrue(FMockHTTP.WasURLCalled(API_FOLDER_SHARED_INCOMING), 'Should call incoming links API');
+end;
+
+procedure TListingProviderTest.TestFetchListing_NormalPath_CallsDirListingAPI;
+var
+	Path: TRealPath;
+	DirListing: TCMRDirItemList;
+	InviteListing: TCMRIncomingInviteList;
+begin
+	FCloud := CreateCloud;
+	FMockHTTP.SetResponse(API_FOLDER, True, JSON_DIR_LISTING);
+	Path.FromPath('\account\myfolder');
+
+	FProvider.FetchListing(FCloud, Path, DirListing, InviteListing);
+
+	Assert.IsTrue(FMockHTTP.WasURLCalled(API_FOLDER), 'Should call folder API');
+end;
+
+procedure TListingProviderTest.TestFetchListing_TrashPath_ReturnsSuccess;
+var
+	Path: TRealPath;
+	DirListing: TCMRDirItemList;
+	InviteListing: TCMRIncomingInviteList;
+	Success: Boolean;
+begin
+	FCloud := CreateCloud;
+	FMockHTTP.SetResponse(API_TRASHBIN, True, JSON_TRASHBIN_LISTING);
+	Path.FromPath('\account' + TrashPostfix);
+
+	Success := FProvider.FetchListing(FCloud, Path, DirListing, InviteListing);
+
+	Assert.IsTrue(Success, 'FetchListing should return True on success');
+end;
+
+procedure TListingProviderTest.TestFetchListing_SharedPath_ReturnsSuccess;
+var
+	Path: TRealPath;
+	DirListing: TCMRDirItemList;
+	InviteListing: TCMRIncomingInviteList;
+	Success: Boolean;
+begin
+	FCloud := CreateCloud;
+	FMockHTTP.SetResponse(API_FOLDER_SHARED_LINKS, True, JSON_SHARED_LINKS_LISTING);
+	Path.FromPath('\account' + SharedPostfix);
+
+	Success := FProvider.FetchListing(FCloud, Path, DirListing, InviteListing);
+
+	Assert.IsTrue(Success, 'FetchListing should return True on success');
+end;
+
+procedure TListingProviderTest.TestFetchListing_InvitesPath_ReturnsSuccess;
+var
+	Path: TRealPath;
+	DirListing: TCMRDirItemList;
+	InviteListing: TCMRIncomingInviteList;
+	Success: Boolean;
+begin
+	FCloud := CreateCloud;
+	FMockHTTP.SetResponse(API_FOLDER_SHARED_INCOMING, True, JSON_INCOMING_INVITES_LISTING);
+	Path.FromPath('\account' + InvitesPostfix);
+
+	Success := FProvider.FetchListing(FCloud, Path, DirListing, InviteListing);
+
+	Assert.IsTrue(Success, 'FetchListing should return True on success');
+end;
+
+procedure TListingProviderTest.TestFetchListing_NormalPath_ReturnsSuccess;
+var
+	Path: TRealPath;
+	DirListing: TCMRDirItemList;
+	InviteListing: TCMRIncomingInviteList;
+	Success: Boolean;
+begin
+	FCloud := CreateCloud;
+	FMockHTTP.SetResponse(API_FOLDER, True, JSON_DIR_LISTING);
+	Path.FromPath('\account\myfolder');
+
+	Success := FProvider.FetchListing(FCloud, Path, DirListing, InviteListing);
+
+	Assert.IsTrue(Success, 'FetchListing should return True on success');
+end;
+
+procedure TListingProviderTest.TestFetchListing_APIFailure_ReturnsFalse;
+var
+	Path: TRealPath;
+	DirListing: TCMRDirItemList;
+	InviteListing: TCMRIncomingInviteList;
+	Success: Boolean;
+begin
+	FCloud := CreateCloud;
+	FMockHTTP.SetResponse(API_FOLDER, False, ''); {HTTP failure}
+	Path.FromPath('\account\myfolder');
+
+	Success := FProvider.FetchListing(FCloud, Path, DirListing, InviteListing);
+
+	Assert.IsFalse(Success, 'FetchListing should return False on HTTP failure');
+end;
+
+procedure TListingProviderTest.TestFetchListing_PopulatesListing;
+var
+	Path: TRealPath;
+	DirListing: TCMRDirItemList;
+	InviteListing: TCMRIncomingInviteList;
+begin
+	FCloud := CreateCloud;
+	FMockHTTP.SetResponse(API_FOLDER, True, JSON_DIR_LISTING);
+	Path.FromPath('\account\myfolder');
+
+	FProvider.FetchListing(FCloud, Path, DirListing, InviteListing);
+
+	Assert.AreEqual(2, Integer(Length(DirListing)), 'Should populate listing with 2 items');
+	Assert.AreEqual('file.txt', DirListing[0].name);
+	Assert.AreEqual('subfolder', DirListing[1].name);
 end;
 
 initialization
