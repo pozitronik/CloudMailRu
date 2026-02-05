24538b61bc0088d8fb719d51f3b659dca92dd1b6
Pozitronik <antikillerxxl@gmail.com>
2026-02-05 18:25:58 +0400

    Changed: extracted shared CloudDirItem parsing logic to eliminate duplication between adapters

--- src/Infrastructure/Adapter/CloudDirItemJsonAdapter.pas
+++ src/Infrastructure/Adapter/CloudDirItemJsonAdapter.pas
@@ -6,7 +6,8 @@ unit CloudDirItemJsonAdapter;
 interface
 
 uses
-	CloudDirItem;
+	CloudDirItem,
+	SafeJSON;
 
 type
 	TCloudDirItemJsonAdapter = class
@@ -16,17 +17,63 @@ type
 		 @param Item Output parameter that receives parsed data
 		 @return True if parsing succeeded, False otherwise}
 		class function Parse(const JSON: WideString; out Item: TCloudDirItem): Boolean; static;
+
+		{Parses item fields from a JSON node into TCloudDirItem record.
+		 Used internally and by CloudDirItemListJsonAdapter to avoid duplication.
+		 @param Node The TSafeJSON node containing item data
+		 @param Item Output parameter that receives parsed data}
+		class procedure ParseFromNode(const Node: TSafeJSON; out Item: TCloudDirItem); static;
 	end;
 
 implementation
 
 uses
-	CloudConstants,
-	SafeJSON;
+	CloudConstants;
+
+class procedure TCloudDirItemJsonAdapter.ParseFromNode(const Node: TSafeJSON; out Item: TCloudDirItem);
+var
+	CountNode: TSafeJSON;
+begin
+	Item := Default(TCloudDirItem);
+
+	Item.size := Node.Get(NAME_SIZE).AsInt64;
+	Item.kind := Node.Get(NAME_KIND).AsString;
+	Item.weblink := Node.Get(NAME_WEBLINK).AsString;
+	Item.type_ := Node.Get(NAME_TYPE).AsString;
+	Item.home := Node.Get(NAME_HOME).AsString;
+	Item.name := Node.Get(NAME_NAME).AsString;
+	Item.visible_name := Item.name;
+
+	{Trash item fields - zero/empty by default from Default(), parsed if present}
+	Item.deleted_at := Node.Get(NAME_DELETED_AT).AsInt;
+	Item.deleted_from := Node.Get(NAME_DELETED_FROM).AsString;
+	Item.deleted_by := Node.Get(NAME_DELETED_BY).AsInt;
+
+	Item.grev := Node.Get(NAME_GREV).AsInt;
+	Item.rev := Node.Get(NAME_REV).AsInt;
+
+	if Item.type_ = TYPE_FILE then
+	begin
+		Item.mtime := Node.Get(NAME_MTIME).AsInt64;
+		Item.virus_scan := Node.Get(NAME_VIRUS_SCAN).AsString;
+		Item.hash := Node.Get(NAME_HASH).AsString;
+	end
+	else
+	begin
+		Item.tree := Node.Get(NAME_TREE).AsString;
+
+		{Parse nested count object - TSafeJSON handles missing gracefully}
+		CountNode := Node.Get(NAME_COUNT);
+		Item.folders_count := CountNode.Get(NAME_FOLDERS).AsInt;
+		Item.files_count := CountNode.Get(NAME_FILES).AsInt;
+
+		Item.mtime := 0;
+	end;
+end;
 
 class function TCloudDirItemJsonAdapter.Parse(const JSON: WideString; out Item: TCloudDirItem): Boolean;
 var
-	Root, Body, CountNode: TSafeJSON;
+	Root, Body: TSafeJSON;
 begin
 	Result := False;
 	Item := Default(TCloudDirItem);
@@ -40,33 +87,7 @@ begin
 		if Body.IsNull then
 			Exit;
 
-		Item.size := Body.Get(NAME_SIZE).AsInt64;
-		Item.kind := Body.Get(NAME_KIND).AsString;
-		Item.weblink := Body.Get(NAME_WEBLINK).AsString;
-		Item.type_ := Body.Get(NAME_TYPE).AsString;
-		Item.home := Body.Get(NAME_HOME).AsString;
-		Item.name := Body.Get(NAME_NAME).AsString;
-
-		if Item.type_ = TYPE_FILE then
-		begin
-			Item.mtime := Body.Get(NAME_MTIME).AsInt64;
-			Item.virus_scan := Body.Get(NAME_VIRUS_SCAN).AsString;
-			Item.hash := Body.Get(NAME_HASH).AsString;
-		end
-		else
-		begin
-			Item.tree := Body.Get(NAME_TREE).AsString;
-			Item.grev := Body.Get(NAME_GREV).AsInt;
-			Item.rev := Body.Get(NAME_REV).AsInt;
-
-			{Parse nested count object - TSafeJSON handles missing gracefully}
-			CountNode := Body.Get(NAME_COUNT);
-			Item.folders_count := CountNode.Get(NAME_FOLDERS).AsInt;
-			Item.files_count := CountNode.Get(NAME_FILES).AsInt;
-
-			Item.mtime := 0;
-		end;
-
+		ParseFromNode(Body, Item);
 		Result := True;
 	finally
 		Root.Free;
--- src/Infrastructure/Adapter/CloudDirItemListJsonAdapter.pas
+++ src/Infrastructure/Adapter/CloudDirItemListJsonAdapter.pas
@@ -30,11 +30,12 @@ implementation
 uses
 	SysUtils,
 	CloudConstants,
-	SafeJSON;
+	SafeJSON,
+	CloudDirItemJsonAdapter;
 
 class function TCloudDirItemListJsonAdapter.Parse(const JSON: WideString; var List: TCloudDirItemList; out ExpectedCount: Integer): Boolean;
 var
-	Root, Body, ListArray, Item, CountNode: TSafeJSON;
+	Root, Body, ListArray, CountNode: TSafeJSON;
 	I: Integer;
 begin
 	Result := False;
@@ -57,43 +58,7 @@ begin
 
 		SetLength(List, ListArray.Count);
 		for I := 0 to ListArray.Count - 1 do
-		begin
-			Item := ListArray.Item(I);
-
-			List[I].size := Item.Get(NAME_SIZE).AsInt64;
-			List[I].kind := Item.Get(NAME_KIND).AsString;
-			List[I].weblink := Item.Get(NAME_WEBLINK).AsString;
-			List[I].type_ := Item.Get(NAME_TYPE).AsString;
-			List[I].home := Item.Get(NAME_HOME).AsString;
-			List[I].name := Item.Get(NAME_NAME).AsString;
-			List[I].visible_name := List[I].name;
-
-			{Trash item fields}
-			List[I].deleted_at := Item.Get(NAME_DELETED_AT).AsInt;
-			List[I].deleted_from := Item.Get(NAME_DELETED_FROM).AsString;
-			List[I].deleted_by := Item.Get(NAME_DELETED_BY).AsInt;
-
-			List[I].grev := Item.Get(NAME_GREV).AsInt;
-			List[I].rev := Item.Get(NAME_REV).AsInt;
-
-			if List[I].type_ = TYPE_FILE then
-			begin
-				List[I].mtime := Item.Get(NAME_MTIME).AsInt64;
-				List[I].virus_scan := Item.Get(NAME_VIRUS_SCAN).AsString;
-				List[I].hash := Item.Get(NAME_HASH).AsString;
-			end
-			else
-			begin
-				List[I].tree := Item.Get(NAME_TREE).AsString;
-
-				{Parse nested count object - TSafeJSON handles missing gracefully}
-				CountNode := Item.Get(NAME_COUNT);
-				List[I].folders_count := CountNode.Get(NAME_FOLDERS).AsInt;
-				List[I].files_count := CountNode.Get(NAME_FILES).AsInt;
-
-				List[I].mtime := 0;
-			end;
-		end;
+			TCloudDirItemJsonAdapter.ParseFromNode(ListArray.Item(I), List[I]);
 
 		Result := True;
 	finally
